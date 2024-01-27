21139c822c82764ecb6883638abaef8f13c3438d
Adnan-Sarkar <adnansarkar111@gmail.com>
2024-01-27 15:08:37 +0600

    Implement rtk-query to login, set user info to the redux store and handle loading state in button.

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,22 +1,78 @@
 import { Button, Form, Input } from "antd";
 import { Controller, FieldValues, useForm } from "react-hook-form";
 import toast from "react-hot-toast";
+import { Link, useNavigate } from "react-router-dom";
+import { useLoginMutation } from "../redux/features/user/userApi";
+import { useState } from "react";
+import { useAppDispatch } from "../redux/hooks";
+import { addUser } from "../redux/features/user/userSlice";
 
 const Login = () => {
+  const [isLoading, setIsLoading] = useState(false);
   const { handleSubmit, control } = useForm();
+  const [login] = useLoginMutation();
+  const dispatch = useAppDispatch();
+  const navigate = useNavigate();
 
-  const onSubmit = (data: FieldValues) => {
+  const onSubmit = async (data: FieldValues) => {
+    setIsLoading(true);
+    const toastId = toast.loading("Loged In...");
     const { email, password } = data;
 
-    if (!email) {
-      toast.error("Email is required", {
+    if (!email && !password) {
+      setIsLoading(false);
+      toast.error("Please provide every information!", {
         duration: 1000,
       });
-    }
-    if (!password) {
-      toast.error("Password is required", {
-        duration: 1000,
-      });
+    } else {
+      try {
+        const response = await login({
+          email,
+          password,
+        }).unwrap();
+
+        if (response?.statusCode !== 200) {
+          throw new Error(response.message);
+        }
+
+        const { user, token } = response.data;
+
+        const userInfo = {
+          _id: user._id,
+          fullName: user.fullName,
+          userName: user.userName,
+          email: user.email,
+          phone: user.phone,
+          gender: user.gender,
+          age: user.age,
+          profileImage: user.profileImage,
+          token,
+        };
+
+        dispatch(addUser(userInfo));
+        setIsLoading(false);
+        toast.success("Loged In Successfull", {
+          id: toastId,
+          duration: 1000,
+        });
+
+        navigate("/dashboard");
+
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      } catch (err: any) {
+        setIsLoading(false);
+        if (err?.status === 404) {
+          toast.error("No User Found!", {
+            id: toastId,
+            duration: 1000,
+          });
+        } else {
+          toast.error(err.message, {
+            id: toastId,
+            duration: 1000,
+          });
+        }
+      }
     }
   };
 
@@ -39,6 +95,7 @@ const Login = () => {
         layout="vertical"
         onFinish={handleSubmit(onSubmit)}
       >
+        <h1 style={{ textAlign: "center" }}>Login</h1>
         <Form.Item label="Email" name="email">
           <Controller
             name="email"
@@ -56,12 +113,20 @@ const Login = () => {
           />
         </Form.Item>
         <Form.Item>
-          <Button type="primary" htmlType="submit" block>
+          <Button
+            type="primary"
+            htmlType="submit"
+            block
+            loading={isLoading}
+            disabled={isLoading}
+          >
             Login
           </Button>
         </Form.Item>
 
-        <div>Don't have any account? Register</div>
+        <div>
+          Don't have any account? <Link to={"/registration"}>Register</Link>
+        </div>
       </Form>
     </div>
   );
