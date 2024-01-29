9b92599af84ceb3eb484f36b6af95f53ba9af715
Adnan-Sarkar <adnansarkar111@gmail.com>
2024-01-29 22:28:03 +0600

    Create product types for component props and product type.

---
+++ src/types/product.types.ts
@@ -0,0 +1,45 @@
+export type TProduct = {
+  name: string;
+  price: number;
+  quantity: number;
+  releaseDate: string;
+  brand: string;
+  model: string;
+  operatingSystem: "iOS" | "Android";
+  storage: {
+    ROM:
+      | "2GB"
+      | "4GB"
+      | "8GB"
+      | "16GB"
+      | "32GB"
+      | "64GB"
+      | "128GB"
+      | "256GB"
+      | "1TB";
+    RAM: "2GB" | "4GB" | "8GB" | "16GB" | "32GB" | "64GB" | "128GB" | "256GB";
+  };
+  screenSize: string;
+  battery: string;
+  camera: {
+    front: string;
+    back: string;
+  };
+  processor: {
+    type: string;
+    speed: string;
+  };
+  rating: number;
+  colors?: string[];
+  chargingType: string;
+  weight: string;
+  details: string;
+  discount: boolean;
+  productImage: string;
+  _id: string;
+};
+
+export type TProductState = {
+  products: TProduct[];
+  totalProducts: number;
+};
