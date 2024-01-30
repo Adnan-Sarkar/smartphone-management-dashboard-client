c68b061520ee8766c7440399e8d068444384e578
Adnan-Sarkar <adnansarkar111@gmail.com>
2024-01-30 20:40:31 +0600

    Implement token verification based on expired time.

--- src/components/layout/ProtechtedRoute.tsx
+++ src/components/layout/ProtechtedRoute.tsx
@@ -1,12 +1,24 @@
-import { ReactNode } from "react";
+import { ReactNode, useEffect, useState } from "react";
 import { useAppSelector } from "../../redux/hooks";
 import { Navigate } from "react-router-dom";
+import { jwtDecode } from "jwt-decode";
 
 const ProtechtedRoute = ({ children }: { children: ReactNode }) => {
   const { token } = useAppSelector((state) => state.user);
+  const [isTokenExpired, setIsTokenExpired] = useState(false);
 
-  if (!token) {
-    return <Navigate to={"/login"} replace={true} />;
+  useEffect(() => {
+    const decode = jwtDecode(token);
+
+    const currentTime = Date.now() / 1000;
+
+    if (decode.exp && decode.exp < currentTime) {
+      setIsTokenExpired(true);
+    }
+  }, [token]);
+
+  if (!token || isTokenExpired) {
+    return <Navigate to="/login" replace={true} />;
   }
 
   return children;
