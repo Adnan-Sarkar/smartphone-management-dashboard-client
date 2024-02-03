7df5b2e488ce81484762c38be9f05f1bd55d28f2
Adnan-Sarkar <adnansarkar111@gmail.com>
2024-02-03 22:46:49 +0600

    Add server url into rtk-query baseUrl.

--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { RootState } from "../store";
 
 const baseQuery = fetchBaseQuery({
-  baseUrl: `http://localhost:5500/api/v1`,
+  baseUrl: `https://smartphone-management-dashboard-server-by-adnan-sarkar.vercel.app/api/v1`,
   credentials: "include",
   prepareHeaders: (headers, { getState }) => {
     const token = (getState() as RootState).user.token;
