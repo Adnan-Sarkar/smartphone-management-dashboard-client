8a40be19fbf53fc7d62e992337843ed819420d51
Adnan-Sarkar <adnansarkar111@gmail.com>
2024-02-03 19:10:28 +0600

    Create query params based on filtering, and set this query string into filter state in redux store and refeching products based on filtering query string. Update Select component of ant design with passing options as a props instead of using Option component.

--- src/components/layout/FilterSidebar.tsx
+++ src/components/layout/FilterSidebar.tsx
@@ -1,43 +1,174 @@
-import { Layout, Slider, DatePicker, Select, Button, Input, Rate } from "antd";
+import {
+  Layout,
+  Slider,
+  DatePicker,
+  Select,
+  Button,
+  Input,
+  Rate,
+  Spin,
+  SliderSingleProps,
+} from "antd";
 import { Controller, FieldValues, useForm } from "react-hook-form";
+import { useGetProductsQuery } from "../../redux/features/product/productApi";
+import { useEffect, useState } from "react";
+import { TProduct } from "../../types/product.types";
+import { useAppDispatch } from "../../redux/hooks";
+import {
+  addProductFilterQuery,
+  removeProductFilterQuery,
+} from "../../redux/features/filter/filterSlice";
 const { Sider } = Layout;
 
 const { RangePicker } = DatePicker;
 const { Option } = Select;
 
+type TFilter = {
+  price: number;
+  screenSize: string[];
+  frontCamera: string[];
+  backCamera: string[];
+};
+
 const FilterSidebar = () => {
+  const [filterInfo, setFilterInfo] = useState<TFilter>();
   const { handleSubmit, control } = useForm();
+  const [query, setQuery] = useState("");
+  const { data, isLoading } = useGetProductsQuery(query);
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    if (!isLoading && data?.data) {
+      const filterObj = data?.data.reduce(
+        (acc: TFilter, cur: TProduct) => {
+          if (cur.price > acc.price) acc.price = cur.price;
+
+          if (!acc.screenSize.includes(cur.screenSize))
+            acc.screenSize.push(cur.screenSize);
+
+          if (!acc.frontCamera.includes(cur.camera.front))
+            acc.frontCamera.push(cur.camera.front);
+
+          if (!acc.backCamera.includes(cur.camera.back))
+            acc.backCamera.push(cur.camera.back);
+
+          return acc;
+        },
+        {
+          price: 0,
+          screenSize: [],
+          frontCamera: [],
+          backCamera: [],
+        }
+      );
+
+      setFilterInfo(filterObj);
+    }
+  }, [isLoading, data]);
 
   const onSubmit = (data: FieldValues) => {
-    // Handle form submission
-    console.log(data);
+    const {
+      price,
+      brand,
+      frontCamera,
+      backCamera,
+      model,
+      os,
+      ram,
+      releaseDate,
+      screenSize,
+      storage,
+      rating,
+    } = data;
 
-    // const {
-    //   price: [minPrice, maxPrice],
-    //   brand,
-    //   frontCamera,
-    //   backCamera,
-    //   model,
-    //   os,
-    //   ram,
-    //   releaseDate: [dateObj1, dateObj2],
-    //   screenSize,
-    //   storage,
-    // } = data;
+    const [dateObj1, dateObj2] = releaseDate || [];
 
-    // let startDate;
-    // let endDateDate;
+    let startDate;
+    let endDateDate;
 
-    // if (dateObj1 && dateObj2) {
-    //   startDate = `${dateObj1.$y}-${dateObj1.$M + 1}-${dateObj1.$D}`;
-    //   endDateDate = `${dateObj2.$y}-${dateObj2.$M + 1}-${dateObj2.$D}`;
-    // }
+    if (dateObj1 && dateObj2) {
+      startDate = `${dateObj1.$y}-${dateObj1.$M + 1}-${dateObj1.$D}`;
+      endDateDate = `${dateObj2.$y}-${dateObj2.$M + 1}-${dateObj2.$D}`;
+    }
 
-    // console.log(startDate, endDateDate);
+    const [minPrice, maxPrice] = price || [];
+
+    // generate query params
+    let queryParams = "";
+    if (minPrice >= 0 && maxPrice) {
+      queryParams = queryParams.concat(`price=${minPrice},${maxPrice}&`);
+    }
+
+    if (startDate && endDateDate) {
+      queryParams = queryParams.concat(
+        `releaseDate=${startDate},${endDateDate}&`
+      );
+    }
+
+    if (brand) {
+      queryParams = queryParams.concat(`brand=${brand}&`);
+    }
+
+    if (os) {
+      queryParams = queryParams.concat(`operatingSystem=${os}&`);
+    }
+
+    if (ram) {
+      queryParams = queryParams.concat(`ram=${ram}&`);
+    }
+
+    if (storage) {
+      queryParams = queryParams.concat(`rom=${storage}&`);
+    }
+
+    if (screenSize) {
+      queryParams = queryParams.concat(`screenSize=${screenSize}&`);
+    }
+
+    if (frontCamera) {
+      queryParams = queryParams.concat(`frontCamera=${frontCamera}&`);
+    }
+
+    if (backCamera) {
+      queryParams = queryParams.concat(`backCamera=${backCamera}&`);
+    }
+
+    if (model) {
+      queryParams = queryParams.concat(`model=${model}&`);
+    }
+
+    if (rating >= 0) {
+      queryParams = queryParams.concat(`rating=${rating}`);
+    }
+
+    dispatch(addProductFilterQuery(queryParams));
+    setQuery(queryParams);
   };
 
   const handleChange = () => {
     handleSubmit(onSubmit)();
+  };
+
+  if (isLoading) return <Spin size="large" />;
+
+  const marks: SliderSingleProps["marks"] = {
+    0: {
+      style: {
+        color: "#f50",
+      },
+      label: <strong>0</strong>,
+    },
+    [filterInfo?.price || 0]: {
+      style: {
+        color: "#f50",
+      },
+      label: <strong>{filterInfo?.price}</strong>,
+    },
+  };
+
+  const handleRemoveFilter = () => {
+    setQuery("");
+    dispatch(removeProductFilterQuery());
   };
 
   return (
@@ -45,12 +176,20 @@ const FilterSidebar = () => {
       <Sider>
         <form onSubmit={handleSubmit(onSubmit)}>
           <div style={{ marginBottom: "10px" }}>
-            <p style={{ fontSize: "18px" }}>Price</p>
+            <p style={{ fontSize: "18px" }}>Price Range</p>
             <Controller
               name="price"
               control={control}
-              defaultValue={[0, 50]}
-              render={({ field }) => <Slider range {...field} />}
+              render={({ field }) => (
+                <Slider
+                  range
+                  {...field}
+                  min={0}
+                  max={filterInfo?.price}
+                  marks={marks}
+                  defaultValue={[0, 0]}
+                />
+              )}
             />
           </div>
 
@@ -69,7 +208,7 @@ const FilterSidebar = () => {
             <Controller
               name="rating"
               control={control}
-              defaultValue={2.5}
+              defaultValue={-1}
               render={({ field }) => (
                 <div onBlur={handleChange}>
                   <Rate allowHalf defaultValue={2.5} {...field} />
@@ -119,6 +258,7 @@ const FilterSidebar = () => {
               render={({ field }) => (
                 <div onBlur={handleChange}>
                   <Select
+                    popupMatchSelectWidth={false}
                     style={{ width: 200 }}
                     placeholder="Filter by operating system"
                     {...field}
@@ -139,21 +279,23 @@ const FilterSidebar = () => {
               defaultValue=""
               render={({ field }) => (
                 <Select
+                  popupMatchSelectWidth={false}
                   style={{ width: 200 }}
                   placeholder="Filter by storage"
                   {...field}
-                >
-                  <Option value="2GB">2GB</Option>
-                  <Option value="4GB">4GB</Option>
-                  <Option value="8GB">8GB</Option>
-                  <Option value="16GB">16GB</Option>
-                  <Option value="32GB">32GB</Option>
-                  <Option value="64GB">64GB</Option>
-                  <Option value="128GB">128GB</Option>
-                  <Option value="512GB">512GB</Option>
-                  <Option value="1TB">1TB</Option>
-                  {/* Add more options as needed */}
-                </Select>
+                  options={[
+                    { value: "2GB", label: <span>2GB</span> },
+                    { value: "4GB", label: <span>4GB</span> },
+                    { value: "8GB", label: <span>8GB</span> },
+                    { value: "16GB", label: <span>16GB</span> },
+                    { value: "32GB", label: <span>32GB</span> },
+                    { value: "64GB", label: <span>64GB</span> },
+                    { value: "128GB", label: <span>128GB</span> },
+                    { value: "256GB", label: <span>256GB</span> },
+                    { value: "512GB", label: <span>512GB</span> },
+                    { value: "1TB", label: <span>1TB</span> },
+                  ]}
+                />
               )}
             />
           </div>
@@ -166,19 +308,21 @@ const FilterSidebar = () => {
               defaultValue=""
               render={({ field }) => (
                 <Select
+                  popupMatchSelectWidth={false}
                   style={{ width: 200 }}
                   placeholder="Filter by RAM"
                   {...field}
-                >
-                  <Option value="2GB">2GB</Option>
-                  <Option value="4GB">4GB</Option>
-                  <Option value="8GB">8GB</Option>
-                  <Option value="16GB">16GB</Option>
-                  <Option value="32GB">32GB</Option>
-                  <Option value="64GB">64GB</Option>
-                  <Option value="128GB">128GB</Option>
-                  {/* Add more options as needed */}
-                </Select>
+                  options={[
+                    { value: "2GB", label: <span>2GB</span> },
+                    { value: "4GB", label: <span>4GB</span> },
+                    { value: "8GB", label: <span>8GB</span> },
+                    { value: "16GB", label: <span>16GB</span> },
+                    { value: "32GB", label: <span>32GB</span> },
+                    { value: "64GB", label: <span>64GB</span> },
+                    { value: "128GB", label: <span>128GB</span> },
+                    { value: "256GB", label: <span>256GB</span> },
+                  ]}
+                />
               )}
             />
           </div>
@@ -191,12 +335,15 @@ const FilterSidebar = () => {
               defaultValue=""
               render={({ field }) => (
                 <Select
+                  popupMatchSelectWidth={false}
                   style={{ width: 200 }}
                   placeholder="Filter by screen size"
                   {...field}
-                >
-                  {/* Add screen size options */}
-                </Select>
+                  options={filterInfo?.screenSize.map((size) => ({
+                    value: size,
+                    label: <span>{size}</span>,
+                  }))}
+                />
               )}
             />
           </div>
@@ -209,12 +356,15 @@ const FilterSidebar = () => {
               defaultValue=""
               render={({ field }) => (
                 <Select
+                  popupMatchSelectWidth={false}
                   style={{ width: 200 }}
                   placeholder="Filter by front camera"
                   {...field}
-                >
-                  {/* Add front camera options */}
-                </Select>
+                  options={filterInfo?.frontCamera.map((size) => ({
+                    value: size,
+                    label: <span>{size}</span>,
+                  }))}
+                />
               )}
             />
           </div>
@@ -227,12 +377,15 @@ const FilterSidebar = () => {
               defaultValue=""
               render={({ field }) => (
                 <Select
+                  popupMatchSelectWidth={false}
                   style={{ width: 200 }}
                   placeholder="Filter by back camera"
                   {...field}
-                >
-                  {/* Add back camera options */}
-                </Select>
+                  options={filterInfo?.backCamera.map((size) => ({
+                    value: size,
+                    label: <span>{size}</span>,
+                  }))}
+                />
               )}
             />
           </div>
@@ -243,6 +396,12 @@ const FilterSidebar = () => {
             </Button>
           </div>
         </form>
+
+        <div style={{ marginTop: "20px" }}>
+          <Button onClick={handleRemoveFilter} block>
+            Remove Filter
+          </Button>
+        </div>
       </Sider>
     </>
   );
