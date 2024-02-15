7cf66468249391dddf47c09679c98c392ffc3dfc
Adnan-Sarkar <adnansarkar111@gmail.com>
2024-02-15 20:42:54 +0600

    Create new components for creating new user and display user list for super admin.

---
+++ src/components/layout/CreateUser.tsx
@@ -0,0 +1,247 @@
+import { Button, Col, Form, Input, Row, Select, Upload } from "antd";
+import { UploadOutlined } from "@ant-design/icons";
+import { Controller, FieldValues, useForm } from "react-hook-form";
+import toast from "react-hot-toast";
+import { useState } from "react";
+import { useRegisterMutation } from "../../redux/features/user/userApi";
+import sendImageToCloudinary from "../../utils/sendImageToCloudinary";
+import DynamicHeader from "./DynamicHeader";
+
+const { Option } = Select;
+
+const CreateUser = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const { handleSubmit, control, reset } = useForm();
+  const [registration] = useRegisterMutation();
+
+  const onSubmit = async (data: FieldValues) => {
+    setIsLoading(true);
+    const toastId = toast.loading("Loading...");
+
+    const {
+      fullName,
+      userName,
+      email,
+      password,
+      phone,
+      gender,
+      age,
+      profileImage,
+      role,
+    } = data;
+
+    if (
+      fullName &&
+      userName &&
+      email &&
+      password &&
+      phone &&
+      gender &&
+      age &&
+      profileImage &&
+      role
+    ) {
+      // send profile image to cloudinary and get the hosted url
+      const imageUrl = await sendImageToCloudinary(profileImage.file);
+
+      try {
+        const response = await registration({
+          fullName,
+          userName,
+          email,
+          password,
+          phone,
+          gender,
+          age: Number(age),
+          profileImage: imageUrl,
+          role,
+        }).unwrap();
+
+        // check registration is successfull or not
+        if (response?.statusCode !== 201) {
+          throw new Error(response.data.message);
+        }
+
+        setIsLoading(false);
+
+        reset();
+
+        toast.success(`Congratulations, ${role} is created successfull`, {
+          id: toastId,
+          duration: 1500,
+        });
+
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      } catch (err: any) {
+        setIsLoading(false);
+        toast.error(err?.data.message, {
+          id: toastId,
+          duration: 1500,
+        });
+      }
+    } else {
+      setIsLoading(false);
+      toast.error("Please provide every information!", {
+        id: toastId,
+        duration: 1500,
+      });
+    }
+  };
+
+  return (
+    <>
+      <DynamicHeader title="Create New User" />
+      <div
+        style={{
+          width: "100%",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Form
+          style={{
+            background: "#f5f6fa",
+            padding: "50px",
+            borderRadius: "10px",
+          }}
+          layout="vertical"
+          onFinish={handleSubmit(onSubmit)}
+        >
+          <h1 style={{ textAlign: "center", marginBottom: "20px" }}>
+            Create New User
+          </h1>
+          <Row gutter={[{ xs: 8, sm: 16, md: 24, lg: 32 }, 8]}>
+            <Col span={12}>
+              <Form.Item label="Full Name" name="fullName">
+                <Controller
+                  name="fullName"
+                  control={control}
+                  render={({ field }) => <Input type="text" {...field} />}
+                />
+              </Form.Item>
+            </Col>
+            <Col span={12}>
+              <Form.Item label="User Name" name="userName">
+                <Controller
+                  name="userName"
+                  control={control}
+                  render={({ field }) => <Input type="text" {...field} />}
+                />
+              </Form.Item>
+            </Col>
+          </Row>
+
+          <Row gutter={[{ xs: 8, sm: 16, md: 24, lg: 32 }, 8]}>
+            <Col span={12}>
+              <Form.Item label="Email" name="email">
+                <Controller
+                  name="email"
+                  control={control}
+                  render={({ field }) => <Input type="email" {...field} />}
+                />
+              </Form.Item>
+            </Col>
+            <Col span={12}>
+              <Form.Item label="Password" name="password">
+                <Controller
+                  name="password"
+                  control={control}
+                  render={({ field }) => (
+                    <Input.Password type="password" {...field} />
+                  )}
+                />
+              </Form.Item>
+            </Col>
+          </Row>
+
+          <Row gutter={[{ xs: 8, sm: 16, md: 24, lg: 32 }, 8]}>
+            <Col span={12}>
+              <Form.Item label="Phone" name="phone">
+                <Controller
+                  name="phone"
+                  control={control}
+                  render={({ field }) => <Input type="text" {...field} />}
+                />
+              </Form.Item>
+            </Col>
+            <Col span={12}>
+              <Form.Item label="Age" name="age">
+                <Controller
+                  name="age"
+                  control={control}
+                  render={({ field }) => <Input type="text" {...field} />}
+                />
+              </Form.Item>
+            </Col>
+          </Row>
+
+          <Row gutter={[{ xs: 8, sm: 16, md: 24, lg: 32 }, 8]}>
+            <Col span={12}>
+              <Form.Item label="Gender" name="gender">
+                <Controller
+                  name="gender"
+                  control={control}
+                  render={({ field }) => (
+                    <Select {...field} placeholder="Gender">
+                      <Option value="male">Male</Option>
+                      <Option value="female">Female</Option>
+                    </Select>
+                  )}
+                />
+              </Form.Item>
+            </Col>
+            <Col span={12}>
+              <Form.Item label="Role" name="role">
+                <Controller
+                  name="role"
+                  control={control}
+                  render={({ field }) => (
+                    <Select {...field} placeholder="Role">
+                      <Option value="branch-manager">Branch Manager</Option>
+                      <Option value="seller">Seller</Option>
+                    </Select>
+                  )}
+                />
+              </Form.Item>
+            </Col>
+          </Row>
+
+          <Row gutter={[{ xs: 8, sm: 16, md: 24, lg: 32 }, 8]}>
+            <Col span={24}>
+              <Form.Item label="Profile Picture" name="profileImage">
+                <Controller
+                  name="profileImage"
+                  control={control}
+                  render={({ field }) => (
+                    <Upload
+                      {...field}
+                      beforeUpload={() => false}
+                      style={{ width: "100%" }}
+                    >
+                      <Button icon={<UploadOutlined />}>Click to Upload</Button>
+                    </Upload>
+                  )}
+                />
+              </Form.Item>
+            </Col>
+          </Row>
+
+          <Form.Item>
+            <Button
+              type="primary"
+              htmlType="submit"
+              block
+              loading={isLoading}
+              disabled={isLoading}
+            >
+              Create User
+            </Button>
+          </Form.Item>
+        </Form>
+      </div>
+    </>
+  );
+};
+
+export default CreateUser;
---
+++ src/components/layout/UserList.tsx
@@ -0,0 +1,167 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { Button, Modal, Space, Table, TableColumnsType, Tag } from "antd";
+import DynamicHeader from "./DynamicHeader";
+import { EditOutlined, DeleteOutlined } from "@ant-design/icons";
+import { useState } from "react";
+import { IUser } from "../../types/user.type";
+import {
+  useDeleteUserMutation,
+  useGetAllUsersQuery,
+} from "../../redux/features/user/userApi";
+import toast from "react-hot-toast";
+import UpdateUserForm from "../form/UpdateUserForm";
+
+const UserList = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [updateUserInfo, setUpdateProductInfo] = useState<IUser | null>(null);
+  const { data, isLoading } = useGetAllUsersQuery("");
+  const [deleteUser, { isLoading: isDeleting }] = useDeleteUserMutation();
+
+  const columns: TableColumnsType<IUser> = [
+    {
+      title: "Image",
+      dataIndex: "profileImage",
+      render: (text) => (
+        <img
+          src={text}
+          alt={text}
+          style={{
+            width: 80,
+            height: 80,
+            objectFit: "cover",
+          }}
+        />
+      ),
+    },
+    {
+      title: "Full Name",
+      dataIndex: "fullName",
+    },
+    {
+      title: "User Role",
+      dataIndex: "role",
+      render: (text) => (
+        <Tag
+          style={{ color: "#222f3e", fontWeight: "bold" }}
+          color={`${text === "seller" ? "#48dbfb" : "#2ed573"}`}
+        >
+          {text}
+        </Tag>
+      ),
+    },
+    {
+      title: "Email",
+      dataIndex: "email",
+    },
+    {
+      title: "Phone Number",
+      dataIndex: "phone",
+    },
+    {
+      title: "Gender",
+      dataIndex: "gender",
+    },
+    {
+      title: "Action",
+      dataIndex: "update",
+      render: (_, record) => (
+        <Space size={"small"} direction="horizontal">
+          <Button
+            onClick={() => handleUpdate(record)}
+            type="text"
+            style={{ background: "#f9ca24" }}
+          >
+            <Space direction="horizontal">
+              <EditOutlined /> Update User
+            </Space>
+          </Button>
+          <Button
+            onClick={() => handleDelete(record._id)}
+            type="text"
+            style={{ background: "#ff7979" }}
+          >
+            <Space direction="horizontal">
+              <DeleteOutlined /> Delete User
+            </Space>
+          </Button>
+        </Space>
+      ),
+    },
+  ];
+
+  let products = [];
+  if (!isLoading && data) {
+    products = data.data.map((user: IUser) => {
+      return {
+        key: user._id,
+        _id: user._id,
+        fullName: user.fullName,
+        userName: user.userName,
+        email: user.email,
+        phone: user.phone,
+        gender: user.gender,
+        age: user.age,
+        profileImage: user.profileImage,
+        role: user.role,
+      };
+    });
+  }
+
+  const handleUpdate = (data: IUser) => {
+    setUpdateProductInfo(data);
+    setIsModalOpen(true);
+  };
+
+  const handleDelete = async (id: string) => {
+    const toastId = toast.loading("Deleting User...");
+
+    console.log({ id });
+
+    try {
+      const response = await deleteUser(id).unwrap();
+
+      if (response.success) {
+        toast.success("User deleted successfully", {
+          id: toastId,
+          duration: 1500,
+        });
+      }
+    } catch (err: any) {
+      toast.error(err.data.errorDetails, {
+        id: toastId,
+        duration: 1500,
+      });
+    }
+  };
+
+  const handleCancel = () => {
+    setIsModalOpen(false);
+  };
+
+  return (
+    <>
+      {isModalOpen && (
+        <Modal open={isModalOpen} onCancel={handleCancel} onOk={handleCancel}>
+          <UpdateUserForm
+            payload={updateUserInfo as IUser}
+            handleCancel={handleCancel}
+          />
+        </Modal>
+      )}
+      <DynamicHeader title="User List" />
+      <div style={{ padding: "10px" }}>
+        <Table
+          loading={isDeleting}
+          columns={columns}
+          dataSource={products}
+          pagination={{
+            position: ["bottomCenter"],
+            pageSize: 6,
+          }}
+        />
+      </div>
+    </>
+  );
+};
+
+export default UserList;
