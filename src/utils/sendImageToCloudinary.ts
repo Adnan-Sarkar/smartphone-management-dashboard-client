12a93f0d934e7df96c61a6cafca1362ddc8fbece
Adnan-Sarkar <adnansarkar111@gmail.com>
2024-05-28 11:27:50 +0600

    Fix some bugs

--- src/utils/sendImageToCloudinary.ts
+++ src/utils/sendImageToCloudinary.ts
@@ -3,15 +3,12 @@ const sendImageToCloudinary = async (imageFile: File) => {
     const formData = new FormData();
     formData.append("file", imageFile);
     formData.append("upload_preset", "smartphone-management");
-    formData.append("cloud_name", "dxljyilvl");
+    formData.append("cloud_name", import.meta.env.VITE_CLOUDINARY_CLOUD_NAME);
 
-    const response = await fetch(
-      "https://api.cloudinary.com/v1_1/dxljyilvl/image/upload",
-      {
-        method: "POST",
-        body: formData,
-      }
-    );
+    const response = await fetch(import.meta.env.VITE_CLOUDINARY_URL, {
+      method: "POST",
+      body: formData,
+    });
 
     const result = await response.json();
     return result?.secure_url;
